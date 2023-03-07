dcf44a8eba4454d92088d8f9fab356b45d5d9278
Rijaia <zaya_89@mail.ru>
2023-03-07 21:09:57 +0300

    All Homework12V3

--- src/lesson11/Book.java
+++ src/lesson11/Book.java
@@ -5,6 +5,20 @@ import java.util.Objects;
 public class Book {
     private int publicationYear;
     private final String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(publicationYear, name, author);
+    }
+
     private Author author;
 
     public Book (String name, int publicationYear) {
@@ -28,16 +42,4 @@ public class Book {
         return "Название книги "+this.name+" Год публикации "+this.publicationYear;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return publicationYear == book.publicationYear && Objects.equals(name, book.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(publicationYear, name);
-    }
 }
